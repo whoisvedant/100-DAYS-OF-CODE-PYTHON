132b5a7636ef3a05fe4d0fba7b87a682d232bb35
Vedant Jadhav <136951168+whoisvedant@users.noreply.github.com>
2023-10-10 18:53:50 +0530

    added Project-2
    This Python Tip Calculator is a simple command-line program that helps you calculate how much each person should contribute towards a bill, including the tip, when dining out with friends or splitting expenses. It makes splitting bills hassle-free by taking care of the math for you.

---
+++ Project-2/project-2.py
@@ -0,0 +1,27 @@
+#If the bill was $150.00, split between 5 people, with 12% tip. 
+
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+#Write your code below this line 👇
+print("Welcome to the tip calculator")
+
+bill = float(input("What was the total bill? $"))
+
+tip = int(input("What tip would you like to give? $"))
+
+people = int(input("How many people to split the bill?"))
+
+tip_as_percent = tip / 100
+
+total_tip_amount = bill * tip_as_percent
+
+total_bill = bill + total_tip_amount
+
+bill_per_person = total_bill / people
+
+final_amount = round(bill_per_person, 2)
+
+print(f"Each person should have to pay : ${final_amount}")
